77f7914245bdf3155cc1dcb455110972d9caa0f1
Apollo <jackson.harris3@gmail.com>
2018-05-12 22:21:00 -0700

    add requred fields

--- src/containers/NewRenterForm.js
+++ src/containers/NewRenterForm.js
@@ -6,9 +6,29 @@ import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import { MenuItem } from 'material-ui/Menu';
 import Input, { InputLabel } from 'material-ui/Input';
+import MaskedInput from 'react-text-mask';
 import { FormLabel, FormGroup, FormControl, FormControlLabel } from 'material-ui/Form';
 //--custom
 const newRenterUrl = 'users/api/newrenter'
+
+function TextMaskCustom(props) {
+    const { inputRef, ...other } = props;
+
+    return (
+        <MaskedInput
+            {...other}
+            ref={inputRef}
+            mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+            placeholderChar={'\u2000'}
+            showMask
+        />
+    );
+}
+
+TextMaskCustom.propTypes = {
+    inputRef: PropTypes.func.isRequired,
+};
+
 
 class NewRenterForm extends Component {
 	constructor(props) {
@@ -19,8 +39,8 @@ class NewRenterForm extends Component {
 			nameId: '',
 			emailId: '',
 			paypalId: '',
-			phoneId: '',
-			addressId: '',
+			phoneId: '(1  )   -    ',
+            addressId: '',
 			isRetnerCreated: false,
 		};
 	}
@@ -68,27 +88,27 @@ class NewRenterForm extends Component {
 		return (
 			<FormControl>
 				<FormGroup>
-					<TextField id="nameId" label="full name" value={this.state.nameId} onChange={this._nameChange} />
+					<TextField required={true} id="nameId" label="full name" value={this.state.nameId} onChange={this._nameChange} />
 				</FormGroup>
 				<br />
 				<br />
 				<FormGroup>
-					<TextField id="emailId" label="email" value={this.state.emailId} onChange={this._nameChange} />
+                    <TextField required={true} id="emailId" label="email" value={this.state.emailId} onChange={this._emailChange} />
 				</FormGroup>
 				<br />
 				<br />
 				<FormGroup>
-					<TextField id="phoneId" label="phone number" value={this.state.phoneId} onChange={this._nameChange} />
+                    <TextField required={true} id="phoneId" label="phone number" value={this.state.phoneId} onChange={this._phoneChange} inputComponent={TextMaskCustom} />
 				</FormGroup>
 				<br />
 				<br />
 				<FormGroup>
-					<TextField id="addressId" label="address" value={this.state.addressId} onChange={this._nameChange} />
+                    <TextField required={true} id="addressId" label="address" value={this.state.addressId} onChange={this._addressChange} />
 				</FormGroup>
 				<br />
 				<br />
 				<FormGroup>
-					<TextField id="paypalId" label="paypal email" value={this.state.paypaylId} onChange={this._nameChange} />
+					<TextField id="paypalId" label="paypal email" value={this.state.paypaylId} onChange={this._paypalChange} />
 				</FormGroup>
 				<br />
 				<br />
@@ -99,7 +119,9 @@ class NewRenterForm extends Component {
 		);
 	}
 }
+
 NewRenterForm.propTypes = {
-    userId: PropTypes.number.isRequired
+    userId: PropTypes.number.isRequired,
 }
+
 export default NewRenterForm;
