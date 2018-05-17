746b90358cbc2b0dbe2d8157303bc026a79fb5c1
Adler11th <mgoftest@gmail.com>
2018-05-17 14:11:48 -0700

    changed boxes look, added slide animation

--- src/components/Theme/StorageShareTheme.jsx
+++ src/components/Theme/StorageShareTheme.jsx
@@ -45,15 +45,15 @@ const StorageShareLight = createMuiTheme({
       default: "#424242"
     },
     primary: {
-      main: "#424242",
-      light: "#ffffff",
-      dark: "#bcbcbc",
-      contrastText: "#000000"
-    },
-    secondary: {
       main: "#388e3c",
       light: "#6abf69",
       dark: "#00600f",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      main: "#f44336",
+      light: "#ff7961",
+      dark: "#ba000d",
       contrastText: "#ffffff"
     }
   },
