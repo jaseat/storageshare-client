8bec78ed91695024058f8e267f2cc6fcc4573bb6
madhavparekh <madhavparekh@gmail.com>
2018-05-17 11:48:24 -0700

    not getting udpated state

--- src/components/Renter/Messages.js
+++ src/components/Renter/Messages.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Tabs, { Tab } from 'material-ui/Tabs';
 
 class Messages extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			value: 0,
+			messages: [],
 		};
 	}
 
 	componentWillMount() {
 		this.props.fetchMessages(this.props.userId);
+		
 	}
 
-	handleChange = (event, value) => {
-		this.setState({ value });
-	};
 	render() {
-		var { value } = this.state;
-		console.log(this.props);
+		console.log('render', this.state.messages);
 		return (
 			<div style={{ width: '100%' }}>
-				<p>Messages</p>
+				{/* {this.state.messages.forEach((e, i) => {
+					return <p>{e.message}</p>;
+				})} */}
 			</div>
 		);
 	}
