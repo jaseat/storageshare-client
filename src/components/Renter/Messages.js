995901f212e9ad0a4a650fadef43e54193130812
madhavparekh <madhavparekh@gmail.com>
2018-05-17 12:36:00 -0700

    adding styling to messages

--- src/components/Renter/Messages.js
+++ src/components/Renter/Messages.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
+import List, { ListItem } from 'material-ui/List';
+
 class Messages extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			messages: [],
-		};
-	}
-
 	componentWillMount() {
 		this.props.fetchMessages(this.props.userId);
-		
 	}
 
 	render() {
-		console.log('render', this.state.messages);
+		console.log('render', this.props.messages);
 		return (
 			<div style={{ width: '100%' }}>
-				{/* {this.state.messages.forEach((e, i) => {
-					return <p>{e.message}</p>;
-				})} */}
+				{this.props.messages &&
+					this.props.messages.map((e, i) => {
+						return (
+							<Paper style={styles.boxes}>
+								<div onClick={this.handleClick} style={styles.icon}>
+								</div>
+								<div style={{ justifySelf: 'center', alignSelf: 'center' }}>
+									{e.message}
+								</div>
+								<Button></Button>
+								<Button style={{ gridColumnStart: '4' }}>Mark as read</Button>
+							</Paper>
+						);
+					})}
 			</div>
 		);
 	}
@@ -30,6 +36,26 @@ class Messages extends Component {
 Messages.propTypes = {
 	fetchMessages: PropTypes.func,
 	messages: PropTypes.array,
+};
+
+var styles = {
+	boxes: {
+		display: 'grid',
+		width: '90%',
+		margin: 'auto',
+		marginTop: '5%',
+		gridTemplateColumns: '[first] 5% 25% auto 15% [end]',
+		gridTemplateRows: '1fr auto',
+		padding: '15px',
+	},
+	collapse: {
+		gridRowStart: '2',
+		gridColumn: 'first / end',
+	},
+	icon: {
+		alignSelf: 'center',
+		cursor: 'pointer',
+	},
 };
 
 export default Messages;
