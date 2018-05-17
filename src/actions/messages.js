b27fd3e3a0c9d3f2bc9879b762de1670a502849c
madhavparekh <madhavparekh@gmail.com>
2018-05-17 00:49:44 -0700

    messages fetch by renter

---
+++ src/actions/messages.js
@@ -0,0 +1,20 @@
+import { MessagesTypes as types } from '../action-types';
+
+const updateMessagesObject = (messages, type) => {
+	return {
+		type,
+		messages,
+	};
+};
+
+export function fetchMessages(id) {
+	return async (dispatch) => {
+		console.log('making a call for messages');
+		var messages = await fetch('/messages/renter/' + id, {
+			credentials: 'same-origin',
+		});
+		messages = await messages.json();
+		console.log(messages);
+		dispatch(updateMessagesObject(messages, types.FETCH_MESSAGES));
+	};
+}
