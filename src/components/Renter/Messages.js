dc177cb32182c81e008225d306b5ae9ccc9f7f57
madhavparekh <madhavparekh@gmail.com>
2018-05-17 14:36:34 -0700

    rendering messages, setting message flag NEW/READ

--- src/components/Renter/Messages.js
+++ src/components/Renter/Messages.js
@@ -4,6 +4,66 @@ import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 import List, { ListItem } from 'material-ui/List';
+import Collapse from 'material-ui/transitions/Collapse';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+
+class Message extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false,
+		};
+	}
+	_renderItems = () => {
+		var lendersResponse = 'Waiting for response a response from lender';
+		if (this.props.message.isAccepted && this.props.message.readByRenter)
+			lendersResponse = `Your package is located at: ${
+				this.props.message.Location.address
+			}`;
+		else if (this.props.message.isAccepted && !this.props.message.readByRenter)
+			lendersResponse = `Congratulation! Property owner of: ${
+				this.props.message.Location.address
+			} has accepted your offer`;
+		else if (this.props.message.isAccepted === false)
+			lendersResponse = `Sorry! Property owner of: ${
+				this.props.message.Location.address
+			} has declined your offer`;
+
+		return (
+			<Collapse in={this.state.open} style={styles.collapse}>
+				<p>{lendersResponse}</p>
+			</Collapse>
+		);
+	};
+
+	handleClick = () => {
+		this.setState({ open: !this.state.open });
+	};
+
+	render() {
+		var { description } = this.props;
+		return (
+			<div>
+				<Paper style={styles.boxes}>
+					<div onClick={this.handleClick} style={styles.icon}>
+						{this.state.open ? <ExpandMore /> : <ChevronRight />}
+					</div>
+					<div style={{ justifySelf: 'left', alignSelf: 'center' }}>
+						Box - {description}
+					</div>
+					{!this.props.message.readByRenter ? (
+						<Button style={{ gridColumnStart: '4' }}>New</Button>
+					) : (
+						<Button style={{ gridColumnStart: '4' }}>Read</Button>
+					)}
+
+					{this._renderItems()}
+				</Paper>
+			</div>
+		);
+	}
+}
 
 class Messages extends Component {
 	componentWillMount() {
@@ -11,23 +71,13 @@ class Messages extends Component {
 	}
 
 	render() {
-		console.log('render', this.props.messages);
+		var { messages } = this.props;
 		return (
 			<div style={{ width: '100%' }}>
-				{this.props.messages &&
-					this.props.messages.map((e, i) => {
-						return (
-							<Paper style={styles.boxes}>
-								<div onClick={this.handleClick} style={styles.icon}>
-								</div>
-								<div style={{ justifySelf: 'center', alignSelf: 'center' }}>
-									{e.message}
-								</div>
-								<Button></Button>
-								<Button style={{ gridColumnStart: '4' }}>Mark as read</Button>
-							</Paper>
-						);
-					})}
+				{messages &&
+					messages.map((m, i) => (
+						<Message key={i} description={m.Box.description} message={m} />
+					))}
 			</div>
 		);
 	}
